88a6bfffd9b8c2662e84cdc512af8c95f8a2fa44
amvalvo <avalvo@ambizmedia.com>
2026-06-10 11:27:22 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,6 @@
+export { default } from 'next-auth/middleware'
 
+export const config = {
+  // Protect everything except login and auth API routes
+  matcher: ['/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)'],
+}
