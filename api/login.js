00562fb79957e1f6d0ff009a0be704cfc4b6dd11
amvalvo <avalvo@ambizmedia.com>
2026-06-10 21:15:40 -0400

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: `${process.env.APP_URL}/auth/callback`,
+    response_type: 'code',
+    scope: 'openid email profile',
+    prompt: 'select_account',
+  })
+  res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params}`)
+}
